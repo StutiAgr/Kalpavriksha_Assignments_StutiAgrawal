3cf1479d1b2599a12e31264697b544bf4d4ef26f
Stuti Agrawal <stuti01.agrawal@gmail.com>
2025-10-01 12:34:45 +0530

    Assignment 2 submission

---
+++ Assignment 2/UserManagement.c
@@ -0,0 +1,179 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+//file where records are stored
+#define FILENAME "users.txt"
+
+//structure to represent user
+typedef struct{
+    int id;
+    char name[100];
+    int age;
+} User;
+
+//functions declaration
+int getNextId();
+void createUser();
+void readUsers();
+void updateUser();
+void deleteUser();
+void menu();
+
+int main(){
+    int choice;
+    do{
+        menu();
+        printf("Enter choice: ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                createUser();
+                break;
+            case 2:
+                readUsers();
+                break;
+            case 3:
+                updateUser();
+                break;
+            case 4:
+                deleteUser();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice.\n");
+        }
+    }while(choice!=5);
+    return 0;
+}
+
+//function to get next unique ID
+//we scan file till the end and pick last id+1
+int getNextId() {
+    FILE *f=fopen(FILENAME, "r");
+    int id=0,age;
+    char name[100];
+    if(!f) return 1; //if file doesn't exist, start ids from 1
+
+    //read each line and overwrite id until end of file
+    while(fscanf(f,"%d\t%99[^\t]\t%d", &id, name, &age)==3);
+    fclose(f);
+    return id+1;
+}
+
+void createUser(){
+    FILE *f=fopen(FILENAME,"a");
+    User u;
+    if(f==NULL){
+        printf("Error: Unable to open file");
+        return;
+    }
+    printf("----Add New User----\n");
+    //because id has to be unique, code will assign it itself
+    u.id=getNextId();
+    printf("Enter name: ");
+    scanf(" %[^\n]", u.name);  //read full line until newline
+    printf("Enter age: ");
+    scanf("%d", &u.age);
+    fprintf(f,"%d\t%s\t%d\n",u.id,u.name,u.age);
+    fclose(f);
+    printf("User added. Assigned ID: %d\n",u.id);
+}
+
+void readUsers(){
+    FILE *f=fopen(FILENAME,"r");
+    if(!f){
+        printf("Unable to open file.\n");
+        return;
+    }
+    int count=0;
+    User u;
+    printf("%-5s %-25s %-5s\n","ID","Name","Age");
+    while(fscanf(f,"%d\t%99[^\t]\t%d",&u.id,u.name,&u.age)==3){
+        printf("%-5d %-25s %-5d\n",u.id,u.name,u.age);
+        count++;
+    }
+    if(count==0) printf("No users found.\n");
+    fclose(f);
+}
+
+//update details of user by searching with id
+//as we cant modify the file, we will copy everythiing to a temp file, only updating matching record
+void updateUser(){
+    int id,found=0;
+    FILE *f=fopen(FILENAME,"r");
+    FILE *temp=fopen("temp.txt","w");
+    if(!f||!temp){
+        printf("Unable to open file\n");
+        return;
+    }
+    printf("Enter ID to update: ");
+    scanf("%d",&id);
+    User u;
+    while(fscanf(f,"%d\t%99[^\t]\t%d",&u.id,u.name,&u.age)==3){
+        if(u.id==id){
+            found=1;
+            //ask for new values;
+            printf("Enter new name: ");
+            scanf(" %[^\n]",u.name);
+            printf("Enter new age: ");
+            scanf("%d",&u.age);
+        }
+        fprintf(temp,"%d\t%s\t%d\n",u.id,u.name,u.age);
+    }
+    fclose(f);
+    fclose(temp);
+    //replace old file with new file
+    remove(FILENAME);
+    rename("temp.txt",FILENAME);
+
+    if(found) printf("User updated.\n");
+    else printf("User not found.\n");
+}
+
+//delete user by id
+//again, copy all records except one with matching id
+void deleteUser(){
+    int id,found=0;
+    FILE *f=fopen(FILENAME,"r");
+    FILE *temp=fopen("temp.txt","w");
+    if(!f||!temp){
+        printf("Unable to open file.");
+        return;
+    }
+    printf("\n----Delete a User----\n");
+    printf("Enter ID to delete: ");
+    scanf("%d",&id);
+    User u;
+    while(fscanf(f,"%d\t%99[^\t]\t%d",&u.id,u.name,&u.age)==3){
+        if(u.id==id){
+            found=1;
+            //skip writing this -> effectively deleting it
+            continue;
+        }
+        fprintf(temp,"%d\t%s\t%d\n",u.id,u.name,u.age);
+    }
+    fclose(f);
+    fclose(temp);
+    remove(FILENAME);
+    rename("temp.txt",FILENAME);
+    
+    if(found) printf("User deleted.\n");
+    else printf("User not found.\n");
+}
+void menu() {
+    printf("\n----User Management----\n");
+    printf("1. Create User\n");
+    printf("2. Read Users\n");
+    printf("3. Update User\n");
+    printf("4. Delete User\n");
+    printf("5. Exit\n");
+}
+/*
+    things handled:
+    -unique ids by assigning it instead of letting user choose it
+    -handling printing if there are no users
+    -handling if the user to update/delete is not found
+*/
